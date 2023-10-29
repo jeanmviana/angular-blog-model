963313ec0d5550af762ed65ac8a0a51570c4e509
jeanmviana <jeanviana.dev@gmail.com>
2023-10-29 15:40:41 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'NOVO HOMEM DE FERRO',
+    description: 'oláKKKKK',
+    photoCover:
+      'https://sm.ign.com/ign_br/news/m/marvels-ir/marvels-iron-man-vr-release-date-now-set-for-july-2020_8h12.jpg',
+  },
+  {
+    id: '2',
+    title: 'saiu novo filme',
+    description: 'olá',
+    photoCover:
+      'https://sm.ign.com/ign_br/news/m/marvels-ir/marvels-iron-man-vr-release-date-now-set-for-july-2020_8h12.jpg',
+  },
+];
